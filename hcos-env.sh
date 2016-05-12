750ef1e6e900176d3624b40de011cade6d705b14
Howard Chen <ibanezchen@gmail.com>
2016-05-12 16:45:15 +0800

    refine hcos-env.sh

--- hcos-env.sh
+++ hcos-env.sh
@@ -1,22 +1,28 @@
+#!/bin/bash
+
 if [ -z "$SOC" ]; then
-	echo "set SOC variable"
+	echo "set SOC variable, e.g. linkit, ameba"
 	return
 fi
+PREFIX=${PREFIX:-../prefix/arm-none-eabi}
+source $PREFIX/bin/cfg-${SOC}.sh
+eval cfg_${SOC}
+
 export CC=arm-none-eabi-gcc
 export AR=arm-none-eabi-ar
 export RL=arm-none-eabi-ar
 
-export PREFIX=${PREFIX:-"`pwd`/../prefix/arm-none-eabi"}
 export DESTDIR=$PREFIX
-source $PREFIX/bin/soc-${SOC}.sh
-export COPTS=${COPTS:-"-march=armv7-a -mthumb"}
-export MOPTS="${COPTS} -fno-builtin -fno-common -ffunction-sections -fdata-sections"
+echo DESTDIR=$DESTDIR
 
-export CFLAGS="${MOPTS} -O2 -g \
+export CFLAGS="${COPTS} -fno-builtin -fno-common -ffunction-sections -fdata-sections -O2 -g \
 ${SOC_INC} \
 -I${PREFIX}/include \
 -D__socklen_t_defined \
 -Dselect=lwip_select \
 "
+echo CFLAGS=$CFLAGS
+
 export LDFLAGS="${MOPTS} -g -nostartfiles -nodefaultlibs -L${PREFIX}/lib -L${PREFIX}/bin/${SOC} -Tram.ld "
 export LDFLAGS="${LDFLAGS} -Wl,--start-group -lhcos -lc -lgcc ${SOC_LIB} -Wl,--end-group"
+echo LDFLAGS=$LDFLAGS
